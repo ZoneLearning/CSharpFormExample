237b74f300bc012530f38c9b1ce72fe5fa3d540b
ZoneLearning <myselflearningzone@gmail.com>
2016-06-24 12:42:20 +0300

    no message

--- MineField/MineField/Form1.Designer.cs
+++ MineField/MineField/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Location = new System.Drawing.Point(21, 53);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(378, 254);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(21, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(215, 26);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(265, 23);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.ReadOnly = true;
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 324);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
     }
 }
 
--- MineField/MineField/Form1.cs
+++ MineField/MineField/Form1.cs
@@ -16,5 +16,96 @@ namespace MineField
         {
             InitializeComponent();
         }
+        int score = 0;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            FillMine(10, 10);
+
+        }
+        void FillMine(int mine, int size)
+        {
+            flowLayoutPanel.Width = size * 30;
+            flowLayoutPanel.Height = size * 30;
+
+            //start butonuna yeniden basınca içindeki kontrolleri temizlemek için.
+            flowLayoutPanel.Controls.Clear();
+
+            int[] mines = new int[mine];
+
+            Random random = new Random();
+            for (int i = 0; i < mine; i++)
+            {
+                int selected = random.Next(0, size * size);
+                if (mines.Contains(selected))
+                {
+                    i--;
+                    continue;
+                }
+                mines[i] = selected;
+            }
+
+            for (int i = 0; i < size * size; i++)
+            {
+                Button btn = new Button();
+                btn.Width = 30;
+                btn.Height = 30;
+                btn.Margin = new Padding(0);
+                //i=0'dan size*size kadar bütün elamanlardan mines eger mayınlı numaralardan olan i'yi içeriyorsa true döndürür, içermiyorsa false döndürür.
+                //Tag özelliği, nesnenin değerini saklamak için kullanılır. = Cep gibi
+                btn.Tag = mines.Contains(i);
+                btn.Click += Btn_Click;
+                flowLayoutPanel.Controls.Add(btn);
+
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button clicked = (Button)sender;
+            bool state = (bool)clicked.Tag;
+            if (state == true)
+            {
+                clicked.BackColor = Color.PaleVioletRed;
+                FinishGame();
+
+            }
+            else
+            {
+                clicked.BackColor = Color.GreenYellow;
+                score++;
+                txtScore.Text = score.ToString();
+            }
+
+
+        }
+
+        void FinishGame()
+        {
+            score = 0;
+
+            foreach (Button item in flowLayoutPanel.Controls)
+            {
+
+                bool state = (bool)item.Tag;
+
+                if (state)
+                {
+                    item.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    item.BackColor = Color.GreenYellow;
+                }
+            }
+            DialogResult result = MessageBox.Show("Do you want to play again?", " Finish Game", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                FillMine(10, 10);
+            }
+
+        }
+
+
     }
 }
