fc3557c3797a822af3568d760aeee05785a56e2d
ZoneLearning <myselflearningzone@gmail.com>
2016-06-24 19:43:54 +0300

    Otobüstek koltuk dizilimi, kapı vs. girişleri margin değerleri kullanılarak yapıldı.

--- OtobusOtomasyonu/OtobusOtomasyonu/Form1.Designer.cs
+++ OtobusOtomasyonu/OtobusOtomasyonu/Form1.Designer.cs
@@ -29,31 +29,84 @@
         private void InitializeComponent()
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.seferlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adanaKayseriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 33);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 416);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 319);
             this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seferlerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(249, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // seferlerToolStripMenuItem
+            // 
+            this.seferlerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adanaKayseriToolStripMenuItem});
+            this.seferlerToolStripMenuItem.Name = "seferlerToolStripMenuItem";
+            this.seferlerToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.seferlerToolStripMenuItem.Text = "Seferler";
+            // 
+            // adanaKayseriToolStripMenuItem
+            // 
+            this.adanaKayseriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.adanaKayseriToolStripMenuItem.Name = "adanaKayseriToolStripMenuItem";
+            this.adanaKayseriToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.adanaKayseriToolStripMenuItem.Text = "Adana-Kayseri";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "07:00",
+            "09:00",
+            "13:00",
+            "20:00"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(231, 441);
+            this.ClientSize = new System.Drawing.Size(249, 404);
             this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem seferlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adanaKayseriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
     }
 }
 
--- OtobusOtomasyonu/OtobusOtomasyonu/Form1.cs
+++ OtobusOtomasyonu/OtobusOtomasyonu/Form1.cs
@@ -19,20 +19,67 @@ namespace OtobusOtomasyonu
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            FillBusSeat(40, 7);
+            FillBusSeat(58, 7);
         }
-  
+
         void FillBusSeat(int seatNumber, int order)
         {
+            //koltuk listesinin temizlenmesini sağlar.
+            flowLayoutPanel1.Controls.Clear();
+
+            //Koltuk sayısı kadar koltuk olusturacak for döngüsü
             for (int i = 1; i <= seatNumber; i++)
             {
-                //Herbirkoltuk birer buton
+                //Her bir koltuk birer buton
                 Button seat = new Button();
-                seat.Width = 50;
-                seat.Height = 50;
-                seat.Margin = new Padding(0);
-                flowLayoutPanel1.Controls.Add(seat); 
+                seat.Width = 40;
+                seat.Height = 40;
+                seat.Text = i.ToString();
+                //son 4'lü yanyana dursun diye sondan 3 önceki elamanı 20 ilerlettik. ( ortalı olsun diye)
+                if (seatNumber - 3 == i) 
+
+                    seat.Margin = new Padding(20, 0, 0, 0);
+
+                else if (seatNumber - 1 == i) //son sıradaki elemanda koridor olmasını engeller
+                    seat.Margin = new Padding(0);
+
+                else if (order * 4 - 2 == i)//orta kapıyı bulup orta kapının karşısına gelen koltuğun sağına koltuk gelmemesini sağlayan koşul                
+                seat.Margin = new Padding(0, 0, 120, 0);
+
+                else if (i % 4 == 3 && i < (order - 1) * 4) //kapıdan çnceki koltuklarda koridor koltuk numarasının 4'e modunun 3 olduğu durumdur.
+
+                    seat.Margin = new Padding(40, 0, 0, 0);
+
+                else if (i % 4 == 1 && i > order * 4)// kapıdan sonra sıra 2 kayar ve mod 4 = 1 olduğu zaman koridor olmalıdır
+
+                    seat.Margin = new Padding(40, 0, 0, 0);
+
+                else // bu koşullardan biri değilse normal koltuktur. Boşluksuz hizalanmalı.
+                    seat.Margin = new Padding(0);
+                //koltugu panele ekledik.
+           flowLayoutPanel1.Controls.Add(seat);
             }
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (toolStripComboBox1.SelectedItem.ToString())
+            {
+                case "07:00":
+                    FillBusSeat(46, 7);
+                break;
+                case "09:00":
+                    FillBusSeat(42, 6);
+                    break;
+                case "13:00":
+                    FillBusSeat(50, 8);
+                    break;
+                case "20:00":
+                    FillBusSeat(54, 8);
+                    break;
+               
+            }
+
         }
     }
 }
